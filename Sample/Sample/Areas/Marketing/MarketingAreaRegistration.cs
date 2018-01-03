cf5e2b600415761164b7fd1f3ae6efd81aab2c78
ngohungphuc <ngohungphuc95@gmail.com>
2018-01-03 23:10:15 +0700

    add sample

---
+++ Sample/Sample/Areas/Marketing/MarketingAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace Sample
+{
+    public class MarketingAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Marketing";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+
+            context.MapRoute(
+
+                "Marketing_default",
+
+                "Marketing/{controller}/{action}/{id}",
+
+                new { controller = "Marketing", action = "Index", id = UrlParameter.Optional },
+
+                new string[] { "Marketing.Controllers" });
+
+        }
+    }
+}
